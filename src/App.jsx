dc6dec7db25233062a178256c543995f0f89f410
bc464 <bclaassen9@gmail.com>
2023-07-24 17:10:09 +0200

    project files and some initial pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ScrollToTop from "./components/ScrollToTop"
+import NoPage from "./pages/NoPage"
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About"
+import Careers from "./pages/Careers"
+import Locations from "./pages/Locations";
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter >
+    <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="careers" element={<Careers />} />
+          <Route path="locations" element={<Locations />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
